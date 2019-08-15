0dba2c6fbf4db4c675c8f7d5dbe92bab518149ad
Amadeusz Wach <amadeusz.wach@scz.pl>
2019-08-15 20:21:33 +0200

    Extensions added

---
+++ Caracan.Pdf/Configuration/PdfGeneratorOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Caracan.Pdf.Configuration
+{
+    public class PdfGeneratorOptions
+    {
+        public string Connection { get; set; }
+    }
+}
---
+++ Caracan.Pdf/Extensions/PdfGeneratorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Caracan.Pdf.Configuration;
+using Caracan.Pdf.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Caracan.Pdf.Extensions
+{
+    public static class PdfGeneratorExtensions
+    {
+        public static IServiceCollection AddPdfGenerator(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<PdfGeneratorOptions>(configuration);
+            services.AddSingleton<IPdfGenerator, PdfGenerator>();
+
+            return services;
+        }
+    }
+}
