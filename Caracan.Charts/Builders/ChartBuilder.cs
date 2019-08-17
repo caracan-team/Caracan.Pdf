66cb96aaead83f9be3ab87a2bbd811b9f08aacbe
Amadeusz Wach <amadeusz.wach@scz.pl>
2019-08-17 17:16:55 +0200

    ChartBuilder added

---
+++ Caracan.Charts/Builders/ChartBuilder.cs
@@ -0,0 +1,29 @@
+﻿using Caracan.Charts.Models;
+using System.Collections.Generic;
+
+namespace Caracan.Charts.Builders
+{
+    public class ChartBuilder : IChartBuilder
+    {
+        private readonly Chart _chart = new Chart();
+
+        public IChartBuilder AddLabels(List<string> labels)
+        {
+            if (_chart.Data is null)
+                _chart.Data = new Data();
+
+            _chart.Data.Labels.AddRange(labels);
+
+            return this;
+        }
+
+        public IChartBuilder ShowArea()
+        {
+            if (_chart.Options is null)
+                _chart.Options = new Options();
+            _chart.Options.ShowArea = true;
+
+            return this;
+        }
+    }
+}
---
+++ Caracan.Charts/Models/Chart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Caracan.Charts.Models
+{
+    public class Chart
+    {
+        public ChartType Type { get; set; }
+        public Data Data { get; set; }
+        public Options Options { get; set; }
+    }
+}
