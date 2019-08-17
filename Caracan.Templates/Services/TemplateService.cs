e45130d271b418b12c0d6e3270bab30b1586a8cb
Amadeusz Wach <amadeusz.wach@scz.pl>
2019-08-17 18:42:33 +0200

    Unit test project added

---
+++ Caracan.Templates/Services/TemplateService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Caracan.Templates.Services
+{
+    public class TemplateService : ITemplateService
+    {
+    }
+}
